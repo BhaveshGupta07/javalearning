29f6e777f07deb7792fe0198d0745b696ddb063f
Bhavesh Gupta <bhuvigupta50@gmail.com>
2023-12-19 14:14:37 +0530

    hello basics

---
+++ hello.java
@@ -0,0 +1,15 @@
+//class name in java is same as the file name
+
+public class hello {
+
+//just an basic example to print the hello message
+public static void main(String[] args) {
+    System.out.println("hello world");
+    System.out.println(44);
+    System.out.println(44.4);
+    System.out.println(44*5);
+}
+/*this is the multi line comment in java */
+
+    
+}
